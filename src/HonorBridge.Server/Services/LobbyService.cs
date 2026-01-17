83a03b505e0369ed78fd6e83c7508eb3ca0af21d
ninadk <ninadsanjaykulkarni@gmail.com>
2026-01-17 18:37:03 +0530

    Fix: Enable bot interactivity by broadcasting StateUpdated events from LobbyService

--- src/HonorBridge.Server/Services/LobbyService.cs
+++ src/HonorBridge.Server/Services/LobbyService.cs
@@ -1,15 +1,33 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 
+using Microsoft.AspNetCore.SignalR;
+using HonorBridge.Server.Hubs;
+
 namespace HonorBridge.Server.Services;
 
 public class LobbyService
 {
     private readonly ConcurrentDictionary<string, GameRoom> _rooms = new();
+    private readonly IHubContext<BridgeHub> _hubContext;
+
+    public LobbyService(IHubContext<BridgeHub> hubContext)
+    {
+        _hubContext = hubContext;
+    }
 
     public GameRoom GetOrCreateRoom(string roomId)
     {
-        return _rooms.GetOrAdd(roomId, id => new GameRoom(id));
+        return _rooms.GetOrAdd(roomId, id => 
+        {
+            var room = new GameRoom(id);
+            // Subscribe to state changes to notify clients
+            room.OnStateChanged += async () => 
+            {
+                await _hubContext.Clients.Group(room.RoomId).SendAsync("StateUpdated");
+            };
+            return room;
+        });
     }
 
     public GameRoom? GetRoom(string roomId)
